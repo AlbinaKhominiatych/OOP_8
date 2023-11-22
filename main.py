7be071eaad86a7aece0cd030b933e3e8cb25622d
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-22 20:19:16 +0200

    Завдання 1 Задайте метаклас, що автоматично додає додатковий функціонал до всіх класів, що його використовують.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+"""Завдання 1
+Задайте метаклас, що автоматично додає
+додатковий функціонал до всіх класів, що його
+використовують."""
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
-        #маніпуляція з класом
-        dct["attr"] = 100
-        print(name)
-        print(bases)
-        print(dct)
+        dct["hello"] = lambda self: print(f"Hello from {self.__class__.__name__}")
         return super().__new__(cls, name, bases, dct)
 
-class MyClass(metaclass=MyMeta):
+class MyClass(metaclass = MyMeta):
     attr = 10
     def metod(self):
         pass
+
 print(dir(MyClass))
 obj = MyClass()
 print(obj.attr)
+obj.hello()
