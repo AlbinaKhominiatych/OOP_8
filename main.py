8e01c9ef6463affc293550ef8d966f7291d8415f
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-22 19:56:43 +0200

    маніпуляція з атрибутами

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 class MyMeta(type):
     def __new__(cls, name, bases, dct):
         #маніпуляція з класом
+        dct["attr"] = 100
+        print(name)
+        print(bases)
+        print(dct)
         return super().__new__(cls, name, bases, dct)
 
 class MyClass(metaclass=MyMeta):
     attr = 10
     def metod(self):
         pass
+print(dir(MyClass))
+obj = MyClass()
+print(obj.attr)
